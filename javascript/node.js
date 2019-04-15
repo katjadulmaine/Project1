93f44003c4f9b33c6826c98d3c3d5fd43853d5b5
Dylan Wright <wright.dylan5@gmail.com>
2019-04-15 15:02:10 -0400

    restarted project

--- javascript/node.js
+++
@@ -1,7 +0,0 @@
-var http = require("hhtp");
-
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Bye World");
-    response.end();
-}).listen(8888);
